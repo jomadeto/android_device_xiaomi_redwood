3d6e24bb5c7b8b1b6af9acfcf3876d01a2531a2c
jomadeto <jomadeto@gmail.com>
2026-02-03 13:14:40 +0100

    Revert "redwood: Move over to AOSP way of shipping kernel"
    This reverts commit 62540e053621aad974b7a696cb83bc223f79665d.

--- device.mk
+++ device.mk
@@ -274,9 +274,6 @@ PRODUCT_COPY_FILES += \
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
-LOCAL_KERNEL := device/xiaomi/redwood-kernel/Image
-PRODUCT_COPY_FILES += \
-	$(LOCAL_KERNEL):kernel
 
 # Lineage Health
 PRODUCT_PACKAGES += \
